fd114c547e9e1d74a6261d9b5d22b5229c0d2358
Ganjali Imanov <ganjali.imanov@gmail.com>
2022-09-25 22:04:56 +0400

    Fix some change

---
+++ Basic/Basic/BasicExamples/ConditionalOperators.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExamples
+{
+    public class ConditionalOperators
+    {
+        public int EvenNumbersSum()
+        {
+            int[] numbers = new int[] { 2, 3, 4, 6 };
+            int summary = 0, count = 0, average;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    count++;
+                    summary += numbers[i];
+                }
+            }
+            average = summary / count;
+            return average;
+        }
+
+        public int FindLongest()
+        {
+            int[] numbersSecond = new int[5] { 12, 15, 68, 55, 55 };
+            int maxNumber = 0;
+            for (int i = 0; i < numbersSecond.Length; i++)
+            {
+                if (numbersSecond[i] > maxNumber)
+                {
+                    maxNumber = numbersSecond[i];
+                }
+            }
+            return maxNumber;
+        }
+
+        public int SumOfDigits(int number)
+        {
+            int sum = 0;
+            if (number > 0)
+            {
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number = number / 10;
+                }
+            }
+            return sum;
+        }
+    }
+}
