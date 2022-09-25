44822d7ece215d516807e94da29f4342b25a69e6
Ganjali717 <ganjali.imanov@gmail.com>
2022-09-25 23:57:45 +0400

    Change some git credentials

--- Basic/Basic/SimpleAPI/Controllers/WeatherForecastController.cs
+++ Basic/Basic/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -6,11 +6,6 @@ namespace SimpleAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
