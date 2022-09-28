811a7cc8ecf029ab6695939a078a755415051e37
Ganjali717 <ganjali.imanov@gmail.com>
2022-09-28 11:53:26 +0400

    Format

--- BP/BP/BasicExamples/Program.cs
+++ BP/BP/BasicExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Examples;
+using System.Globalization;
 using System.Text;
 using static Examples.Delegate;
 
@@ -48,7 +49,7 @@ for (int j = 0; j < strB.Length; j++)
 #endregion
 
 #region DateTimes
-TimeSpan nearlyTenDays = TimeSpan.FromDays(10) - TimeSpan.FromSeconds(1);
+/*TimeSpan nearlyTenDays = TimeSpan.FromDays(10) - TimeSpan.FromSeconds(1);
 Console.WriteLine(nearlyTenDays.Days); // 9
 Console.WriteLine(DateTime.MinValue);
 Console.WriteLine(DateTime.MaxValue);
@@ -57,5 +58,24 @@ DateTime dt = new DateTime(2000, 2, 3,
     10, 20, 30);
 DateTime bakuTime = new DateTime(2022, 09, 28, 10, 05, 54);
 string dateFormat = bakuTime.ToString("dd.MM.yyyy"); // We can change format of date
-Console.WriteLine(dateFormat);
+Console.WriteLine(dateFormat);*/
+#endregion
+
+#region Parse
+string s = true.ToString(); // s = "True"
+bool b = bool.Parse(s); // b = true
+Console.WriteLine(b);
+
+bool failure = int.TryParse("qwerty", out int i1);
+bool success = int.TryParse("123", out int i2);
+i1 = 4;
+i2 = 5;
+Console.WriteLine(failure);
+Console.WriteLine(success);
+#endregion
+
+#region Format
+NumberFormatInfo f = new NumberFormatInfo();
+f.CurrencySymbol = "$$";
+Console.WriteLine(3.ToString("C", f));
 #endregion
