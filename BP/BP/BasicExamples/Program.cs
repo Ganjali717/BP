38b926897241d81c755f316bec95d91bfb81859f
Ganjali717 <ganjali.imanov@gmail.com>
2022-09-28 16:37:14 +0400

    IEnumerator methods

--- BP/BP/BasicExamples/Program.cs
+++ BP/BP/BasicExamples/Program.cs
@@ -1,4 +1,5 @@
-﻿using Examples;
+﻿using System.Collections;
+using Examples;
 using System.Globalization;
 using System.Text;
 using static Examples.Delegate;
@@ -81,39 +82,54 @@ Console.WriteLine(3.ToString("C", f));*/
 #endregion
 
 #region Collections
-int[] array1 = { 1, 2 };
+/*int[] array1 = { 1, 2 };
 int[] array2 = { 3, 4 };
 int[] array3 = array1;
 Console.WriteLine($"array1==array2 ? {array1 == array2}"); // The are not equal because they have different memory locations (FALSE)
 Console.WriteLine($"array2==array3 ? {array2 == array3}"); 
-Console.WriteLine($"array3==array1 ? {array3 == array1}");
+Console.WriteLine($"array3==array1 ? {array3 == array1}");*/
 
-var people = new List<string>() { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam" };
+/*var people = new List<string>() { "Eugene", "Mike", "Kate", "Tom", "Bob", "Sam" };
+
 //Add to list 
 people.Add("Bob");
 people.AddRange(new[] { "Sam", "Alice" }); 
 people.Insert(0, "Eugene");
-people.InsertRange(1, new string[] { "Mike", "Kate" });
-//Remove from list
+people.InsertRange(1, new string[] { "Mike", "Kate" });*/
+
+/*//Remove from list
 people.RemoveAt(1); 
 people.Remove("Tom");
 people.RemoveAll(person => person.Length == 3);
 people.RemoveRange(1, 2);
-people.Clear();
-//Search and check element
+people.Clear();*/
+
+/*//Search and check element
 var containsBob = people.Contains("Bob");     //  true
 var containsBill = people.Contains("Bill");    // false
 var existsLength3 = people.Exists(p => p.Length == 3);  // true
 var existsLength7 = people.Exists(p => p.Length == 7);  // false
 var firstWithLength3 = people.Find(p => p.Length == 3); // Tom
 var lastWithLength3 = people.FindLast(p => p.Length == 3);  // Sam
-List<string> peopleWithLength3 = people.FindAll(p => p.Length == 3);
-//Get range and copy to array
+List<string> peopleWithLength3 = people.FindAll(p => p.Length == 3);*/
+
+/*//Get range and copy to array
 var range = people.GetRange(1, 3);
 string[] partOfPeople = new string[3];
-people.CopyTo(0, partOfPeople, 0, 3);
-//Reverse
+people.CopyTo(0, partOfPeople, 0, 3);*/
+
+/*//Reverse
 people.Reverse();
 var people2 = new List<string>() { "Eugene", "Tom", "Mike", "Sam", "Bob" };
-people2.Reverse(1, 3);
+people2.Reverse(1, 3);*/
+
+
+string[] people3 = { "Tom", "Sam", "Bob" };
+IEnumerator peopleEnumerator = people3.GetEnumerator(); // get IEnumerator
+while (peopleEnumerator.MoveNext())   // until returned false
+{
+    string item = (string)peopleEnumerator.Current; // get element at current position
+    Console.WriteLine(item);
+}
+peopleEnumerator.Reset();
 #endregion
